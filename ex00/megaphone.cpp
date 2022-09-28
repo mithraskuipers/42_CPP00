a4b80e5eef85f485ee0a3d918a7c87ede39456f1
Mithras Kuipers <mithras.kuipers@neurolytics.ai>
2022-09-28 18:22:08 +0200

    ex00 done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   megaphone.cpp                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/09/28 18:07:19 by mikuiper      #+#    #+#                 */
+/*   Updated: 2022/09/28 18:18:17 by mikuiper      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int			main(int argc, char **argv)
+{
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; argv[i]; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+			{
+				std::cout << static_cast<char>(std::toupper(argv[i][j]));
+			}
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
