bf6b8add60ed0f3a7956de332071b2c3fbc886d8
Mithras Kuipers <mikuiper@f0r3s16.codam.nl>
2022-10-11 18:17:07 +0200

    grrr

--- ex01/includes/contacts.hpp
+++ ex01/includes/contacts.hpp
@@ -6,29 +6,28 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/10 11:24:14 by mikuiper      #+#    #+#                 */
-/*   Updated: 2022/10/10 12:02:21 by mikuiper      ########   odam.nl         */
+/*   Updated: 2022/10/11 17:44:58 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACTS_HPP
 # define CONTACTS_HPP
 
-# include "../includes/libraries.hpp"
+#include "../includes/libraries.hpp"
 
 class Contact
 {
 	public:
 		Contact(void) {};
 		~Contact(void) {};
-		void	add_contact(void);
+		void	input_contact(void);
 		void	show_contact(void);
 		void	print_contact(void);
-	private:
-		std::string	_FirstName;
-		std::string	_LastName;
-		std::string	_NickName;
-		std::string	_PhoneNumber;
-		std::string	_Secret;
+		std::string	FirstName;
+		std::string	LastName;
+		std::string	NickName;
+		std::string	PhoneNumber;
+		std::string	Secret;
 };
 
 #endif
--- ex01/includes/phonebook.hpp
+++ ex01/includes/phonebook.hpp
@@ -6,24 +6,29 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/10 11:24:39 by mikuiper      #+#    #+#                 */
-/*   Updated: 2022/10/10 11:57:33 by mikuiper      ########   odam.nl         */
+/*   Updated: 2022/10/11 17:32:09 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
+# define MAX_N_CONTACTS 3
+#include "../includes/contacts.hpp"
+
 class PhoneBook
 {
 	public:
-		PhoneBook(void) {};
-		~PhoneBook(void) {};
+		PhoneBook();
+		~PhoneBook();
 		void	add_contact(void);
 		void	search_contact(void);
 		void	show_phonebook(void);
+		void	init_phonebook(void);
+		void	print_phonebook(void);
 		Contact	get_contact(int contact_nbr);
 	private:
-		Contact _contacts[8];
+		Contact _contacts[MAX_N_CONTACTS];
 		int		_nbr_of_contacts;
 };
 
